418a513a14cacd8c5f8baff1a0a24d3f638445d9
Giriharan <giriharan.ravichandran@ni.com>
2024-08-08 14:12:48 +0530

    feat: support for other datatypes for ui creation

--- src/ni_measurement_plugin_converter/utils/_create_measui_file.py
+++ src/ni_measurement_plugin_converter/utils/_create_measui_file.py
@@ -9,6 +9,7 @@ from ni_measurement_ui_creator.constants import (
     NUMERIC_DATA_TYPE_NAMES,
     DataType,
     MeasUIElementPosition,
+    SpecializedDataType,
 )
 from ni_measurement_ui_creator.models import DataElement
 from ni_measurement_ui_creator.utils._create_measui import create_measui
@@ -18,6 +19,8 @@ from ni_measurement_ui_creator.utils._helpers import (
 )
 
 from ni_measurement_plugin_converter.models import InputInfo, OutputInfo
+
+PIN_NAMES = "pin_names"
 
 
 def get_input_data_elements(inputs: List[InputInfo]) -> List[DataElement]:
@@ -29,11 +32,24 @@ def get_input_data_elements(inputs: List[InputInfo]) -> List[DataElement]:
     Returns:
         List[DataElement]: List of data element for input UI compenents.
     """
-    input_data_elements = []
-    top_alignment = MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE
+    input_data_elements = [
+        DataElement(
+            client_id=CLIENT_ID,
+            value_type=SpecializedDataType.IORESOURCE_ARR,
+            left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
+            top_alignment=MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE,
+            name=PIN_NAMES,
+        )
+    ]
+    top_alignment = (
+        MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE
+        + MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+    )
 
     for input in inputs:
         value_type = input.nims_type.split(".")[2]
+        value_type = DataType.Single.name if value_type == nims.DataType.Float.name else value_type
+
         if value_type in NUMERIC_DATA_TYPE_NAMES:
             input_data_elements.append(
                 DataElement(
@@ -41,6 +57,18 @@ def get_input_data_elements(inputs: List[InputInfo]) -> List[DataElement]:
                     left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
                     top_alignment=top_alignment,
                     value_type=value_type,
+                    name=input.param_name,
+                )
+            )
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+
+        elif value_type == nims.DataType.String.name:
+            input_data_elements.append(
+                DataElement(
+                    client_id=CLIENT_ID,
+                    left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
+                    top_alignment=top_alignment,
+                    value_type=DataType.Single.name,
                     name=input.param_name,
                 )
             )
@@ -71,6 +99,19 @@ def get_input_data_elements(inputs: List[InputInfo]) -> List[DataElement]:
             )
             top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
 
+        elif value_type == nims.DataType.FloatArray1D.name:
+            input_data_elements.append(
+                DataElement(
+                    client_id=CLIENT_ID,
+                    left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
+                    top_alignment=top_alignment,
+                    value_type=DataType.Single.name,
+                    name=input.param_name,
+                    is_array=True,
+                )
+            )
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+
     return input_data_elements
 
 
@@ -92,6 +133,8 @@ def get_output_data_elements(outputs: List[OutputInfo]) -> List[DataElement]:
 
     for output in outputs:
         value_type = output.nims_type.split(".")[2]
+        value_type = DataType.Single.name if value_type == nims.DataType.Float.name else value_type
+
         if value_type in NUMERIC_DATA_TYPE_NAMES:
             output_data_elements.append(
                 DataElement(
@@ -99,6 +142,18 @@ def get_output_data_elements(outputs: List[OutputInfo]) -> List[DataElement]:
                     left_alignment=left_alignment,
                     top_alignment=top_alignment,
                     value_type=value_type,
+                    name=output.variable_name,
+                )
+            )
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+
+        elif value_type == nims.DataType.String.name:
+            output_data_elements.append(
+                DataElement(
+                    client_id=CLIENT_ID,
+                    left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
+                    top_alignment=top_alignment,
+                    value_type=DataType.Single.name,
                     name=output.variable_name,
                 )
             )
@@ -123,6 +178,19 @@ def get_output_data_elements(outputs: List[OutputInfo]) -> List[DataElement]:
                     left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
                     top_alignment=top_alignment,
                     value_type=value_type,
+                    name=output.variable_name,
+                    is_array=True,
+                )
+            )
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+
+        elif value_type == nims.DataType.FloatArray1D.name:
+            output_data_elements.append(
+                DataElement(
+                    client_id=CLIENT_ID,
+                    left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
+                    top_alignment=top_alignment,
+                    value_type=DataType.Single.name,
                     name=output.variable_name,
                     is_array=True,
                 )
