7c6087dc0667c73cf97ea4fb8ef7232b6da7990a
Aravind KM <75130529+aravind-70@users.noreply.github.com>
2024-08-19 18:28:58 +0530

    Refactor UI Spacing (#14)
    What does this Pull Request accomplish?
    This pull request accomplishes adding code changes to adjust the UI spacings.
    
    Why should this Pull Request be merged?
    The generated measui file has many vertical and horizontal spacing between controls and indicators. This pull request contains the code changes to adjust the spacing according to the UI elements.
    
    What testing has been done?
    Manual testing.

--- src/ni_measurement_plugin_converter/utils/_create_measui_file.py
+++ src/ni_measurement_plugin_converter/utils/_create_measui_file.py
@@ -39,36 +39,59 @@ def get_input_data_elements(inputs: List[InputInfo]) -> List[DataElement]:
             name=PIN_NAMES,
         )
     ]
+
     top_alignment = (
         MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE
         + MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
     )
 
-    for input_data in inputs:
-        value_type = input_data.nims_type.split(".")[2]
+    for index, input_info in enumerate(inputs):
+        value_type = input_info.nims_type.split(".")[2]
         is_array = False
+
+        height = MeasUIElementPosition.DEFAULT_HEIGHT
+        width = MeasUIElementPosition.DEFAULT_WIDTH
 
         if value_type not in SUPPORTED_NIMS_DATATYPES:
             continue
 
+        if index > 0 and inputs[index - 1].nims_type in [
+            "nims.DataType.Boolean",
+            "nims.DataType.Int64Array1D",
+            "nims.DataType.DoubleArray1D",
+        ]:
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_ADDITIONAL_INCREMENTAL_VALUE
+
         if value_type == nims.DataType.Double.name:
             value_type = DataType.Double.name
+
+        elif value_type == nims.DataType.Boolean.name:
+            height = MeasUIElementPosition.BOOLEAN_HEIGHT
+            width = MeasUIElementPosition.BOOLEAN_WIDTH
 
         elif value_type == nims.DataType.Int64Array1D.name:
             value_type = DataType.Int64.name
             is_array = True
 
+            height = MeasUIElementPosition.ARRAY_HEIGHT
+            width = MeasUIElementPosition.ARRAY_WIDTH
+
         elif value_type == nims.DataType.DoubleArray1D.name:
             value_type = DataType.Double.name
             is_array = True
+
+            height = MeasUIElementPosition.ARRAY_HEIGHT
+            width = MeasUIElementPosition.ARRAY_WIDTH
 
         input_data_elements.append(
             DataElement(
                 client_id=CLIENT_ID,
                 left_alignment=MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE,
                 top_alignment=top_alignment,
+                height=height,
+                width=width,
                 value_type=value_type,
-                name=input_data.param_name,
+                name=input_info.param_name,
                 is_array=is_array,
             )
         )
@@ -87,35 +110,60 @@ def get_output_data_elements(outputs: List[OutputInfo]) -> List[DataElement]:
         List[DataElement]: List of data element for output UI components.
     """
     output_data_elements = []
-    top_alignment = MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE
+    top_alignment = (
+        MeasUIElementPosition.TOP_ALIGNMENT_START_VALUE
+        + MeasUIElementPosition.TOP_ALIGNMENT_INCREMENTAL_VALUE
+    )
     left_alignment = (
         MeasUIElementPosition.LEFT_ALIGNMENT_START_VALUE
         + MeasUIElementPosition.LEFT_ALIGNMENT_INCREMENTAL_VALUE
     )
 
-    for output in outputs:
+    for index, output in enumerate(outputs):
         value_type = output.nims_type.split(".")[2]
         is_array = False
+
+        height = MeasUIElementPosition.DEFAULT_HEIGHT
+        width = MeasUIElementPosition.DEFAULT_WIDTH
 
         if value_type not in SUPPORTED_NIMS_DATATYPES:
             continue
 
+        if index > 0 and outputs[index - 1].nims_type in [
+            "nims.DataType.Boolean",
+            "nims.DataType.Int64Array1D",
+            "nims.DataType.DoubleArray1D",
+        ]:
+            top_alignment += MeasUIElementPosition.TOP_ALIGNMENT_ADDITIONAL_INCREMENTAL_VALUE
+
         if value_type == nims.DataType.Double.name:
             value_type = DataType.Double.name
+
+        elif value_type == nims.DataType.Boolean.name:
+            height = MeasUIElementPosition.BOOLEAN_HEIGHT
+            width = MeasUIElementPosition.BOOLEAN_WIDTH
 
         elif value_type == nims.DataType.Int64Array1D.name:
             value_type = DataType.Int64.name
             is_array = True
 
+            height = MeasUIElementPosition.ARRAY_HEIGHT
+            width = MeasUIElementPosition.ARRAY_WIDTH
+
         elif value_type == nims.DataType.DoubleArray1D.name:
             value_type = DataType.Double.name
             is_array = True
+
+            height = MeasUIElementPosition.ARRAY_HEIGHT
+            width = MeasUIElementPosition.ARRAY_WIDTH
 
         output_data_elements.append(
             DataElement(
                 client_id=CLIENT_ID,
                 left_alignment=left_alignment,
                 top_alignment=top_alignment,
+                height=height,
+                width=width,
                 value_type=value_type,
                 name=output.variable_name,
                 is_array=is_array,
