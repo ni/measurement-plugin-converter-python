119ec34e19e256fa46e795f1fc6a108cf9f68a3c
Aravind KM <75130529+aravind-70@users.noreply.github.com>
2024-09-26 18:29:40 +0530

    NI Measurement UI Creator | Update UI (#22)
    What does this Pull Request accomplish?
    This pull request accomplishes the updation of Meas UI file.
    
    Why should this Pull Request be merged?
    This pull request should be merged so that the Measurement UI creator is able to update the UI file as well as generate UI if create is the command call.
    
    What testing has been done?
    Manual testing.

--- ni-measurement-ui-creator/src/ni_measurement_ui_creator/constants/_cli.py
+++ ni-measurement-ui-creator/src/ni_measurement_ui_creator/constants/_cli.py
@@ -3,12 +3,6 @@
 CLI_CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
 
-class CliHelpMessage:
-    """CLI help messages for arguments."""
-
-    OUTPUT_FOLDER = "Output directory path where the .measui file is created."
-
-
 class UserMessage:
     """Messages displayed."""
 
@@ -17,18 +11,36 @@ class UserMessage:
 
     GET_ACTIVE_MEASUREMENTS = "Getting the active measurements..."
     NO_MEASUREMENTS_RUNNING = "No measurement services are running."
+
     AVAILABLE_MEASUREMENTS = "Available services:"
+    AVAILABLE_MEASUI_FILES = "Available Measurement UI Files:"
+
+    SELECT_MEASUREMENT = (
+        "Select a measurement service index ({start}-{end}) to update/generate measui file: "
+    )
+    SELECT_MEASUI_FILE = "Select a measurement UI file index ({start}-{end}) to update: "
 
     INVALID_MEASUREMENT_CHOICE = "Invalid measurement plug-in selected."
-    SELECT_MEASUREMENT = (
-        "Select a measurement service index ({start}-{end}) to generate measui file: "
+    INVALID_MEASUI_CHOICE = "Invalid measurement UI selected."
+
+    NO_MEASUI_FILE = (
+        "No Measurement UI file available. "
+        "Creating a new measui file for the selected measurement..."
+    )
+    INVALID_MEASUI_FILE = (
+        "Invalid Measurement UI file. Creating a new measui file for the selected measurement..."
     )
 
-    CREATED_UI = "Measurement UI created successfully at {filepath}"
+    BINDING_ELEMENTS = "Binding UI controls and indicators..."
+    INPUTS_BOUND = "Inputs are bound successfully."
+    OUTPUTS_BOUND = "Outputs are bound successfully."
+
+    CREATING_ELEMENTS = "Creating new controls and indicators..."
     CREATING_FILE = "Creating Measurement UI..."
+    CREATED_UI = "Measurement UI created successfully at {filepath}"
+    UPDATED_UI = "Measurement UI updated successfully. Please find at {filepath}"
 
     RENDER_TEMPLATE_ERROR = "Error occurred while rendering measui template."
-
     ERROR_OCCURRED = "Error occurred. Please find the log file at {log_file}"
-    ABORTED = "Aborted!"
+
     PROCESS_COMPLETED = "Process completed."
--- ni-measurement-ui-creator/src/ni_measurement_ui_creator/utils/_create_measui.py
+++ ni-measurement-ui-creator/src/ni_measurement_ui_creator/utils/_create_measui.py
@@ -1,19 +1,84 @@
 """Create `.measui` file for the measurements."""
 
 import os
+from logging import getLogger
+from typing import Union
 
 from mako.template import Template
+from ni_measurement_plugin_sdk_service._internal.stubs.ni.measurementlink.measurement.v1.measurement_service_pb2 import (
+    GetMetadataResponse as V1MetaData,
+)
+from ni_measurement_plugin_sdk_service._internal.stubs.ni.measurementlink.measurement.v2.measurement_service_pb2 import (
+    GetMetadataResponse as V2MetaData,
+)
 
-from ni_measurement_ui_creator.constants import CLIENT_ID, ENCODING
+from ni_measurement_ui_creator.constants import CLIENT_ID, LOGGER, UserMessage, MeasUIFile
+from ni_measurement_ui_creator.utils._ui_elements import (
+    create_input_elements_from_client,
+    create_output_elements_from_client,
+)
 
 
-def render_template(
+def create_measui(metadata: Union[V1MetaData, V2MetaData], output_dir: str) -> None:
+    """Create measurement UI file.
+
+    1. Get inputs and outputs from the metadata.
+    2. Create input elements.
+    3. Create output elements.
+
+    Args:
+        metadata (Union[V1MetaData, V2MetaData]): Metadata of a measurement plug-in.
+        output_dir (str): Output directory.
+    """
+    logger = getLogger(LOGGER)
+    inputs = metadata.measurement_signature.configuration_parameters
+    input_elements, _ = create_input_elements_from_client(inputs=inputs)
+
+    outputs = metadata.measurement_signature.outputs
+    output_elements = create_output_elements_from_client(outputs=outputs)
+
+    measui_path = os.path.join(output_dir, metadata.measurement_details.display_name)
+
+    __create_measui(
+        filepath=measui_path,
+        input_output_elements=input_elements + output_elements,
+    )
+    filepath = os.path.abspath(f"{measui_path}{MeasUIFile.MEASUREMENT_UI_FILE_EXTENSION}")
+    logger.info(UserMessage.CREATED_UI.format(filepath=os.path.abspath(filepath)))
+
+
+def __create_measui(filepath: str, input_output_elements: str) -> None:
+    """Create `measui` file.
+
+    Args:
+        filepath (str): MeasUI File Path.
+        input_output_elements (str): Input and Output XML tags.
+    """
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    template_file_path = os.path.join(
+        os.path.dirname(current_dir),
+        "templates",
+        "measurement.measui.mako",
+    )
+
+    file_content = __render_template(
+        template_name=template_file_path,
+        client_id=CLIENT_ID,
+        display_name=os.path.basename(filepath),
+        input_output_elements=input_output_elements,
+    )
+
+    with open(f"{filepath}{MeasUIFile.MEASUREMENT_UI_FILE_EXTENSION}", "wb") as f:
+        f.write(file_content)
+
+
+def __render_template(
     template_name: str,
     client_id: str,
     display_name: str,
     input_output_elements: str,
 ) -> bytes:
-    """Render measui mako file template.
+    """Render `measui` mako file template.
 
     Args:
         template_name (str): Name of mako file.
@@ -24,38 +89,14 @@ def render_template(
     Returns:
         bytes: MeasUI file content.
     """
-    template = Template(filename=template_name, input_encoding=ENCODING, output_encoding=ENCODING)
+    template = Template(
+        filename=template_name,
+        input_encoding=MeasUIFile.ENCODING,
+        output_encoding=MeasUIFile.ENCODING,
+    )
 
     return template.render(
         client_id=client_id,
         display_name=display_name,
         input_output_elements=input_output_elements,
     )
-
-
-def create_measui(filepath: str, input_output_elements: str) -> None:
-    """Create `measui` file.
-
-    Args:
-        filepath (str): MeasUI File Path.
-        input_output_elements (str): Input and Output XML tags.
-
-    Returns:
-        None.
-    """
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    template_file_path = os.path.join(
-        os.path.dirname(current_dir), "templates", "measurement.measui.mako"
-    )
-
-    file_content = render_template(
-        template_name=template_file_path,
-        client_id=CLIENT_ID,
-        display_name=os.path.basename(filepath),
-        input_output_elements=input_output_elements,
-    )
-
-    with open(f"{filepath}.measui", "wb") as f:
-        f.write(file_content)
-
-    return None
